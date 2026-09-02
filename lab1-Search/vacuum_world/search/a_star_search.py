76172cf76101de84af599c6f64a96db8920ff054
ollehaakansson <ollha403@asgard1-402.ad.liu.se>
2026-09-02 16:47:06 +0200

    Added heuristic A*

--- lab1-Search/vacuum_world/search/a_star_search.py
+++ lab1-Search/vacuum_world/search/a_star_search.py
@@ -1,4 +1,5 @@
 from typing import List
+import heapq
 from vacuum_world.search.search_node import SearchNode
 from vacuum_world.search.problem import SearchProblem
 from vacuum_world.world.grid_pos import GridPos
@@ -6,10 +7,15 @@ from .base_search import BaseSearch
 
 
 class AStarNode(SearchNode):    
-    # TODO: Implement this class
+    # A* node with heuristic cost
+    def __init__(self, state, parent=None, cost=0.0, heuristic=0.0):
+        super().__init__(state, parent, None, cost)
+        self.heuristic = heuristic
     
+    # Compare nodes by f(n) = g(n) + h(n)
+    # g(n) = cost[n], h(n) = heuristic[n]
     def __lt__(self, other):
-        return True
+        return (self.cost + self.heuristic) < (other.cost + other.heuristic)
 
 
 class AStarSearch(BaseSearch):
@@ -18,15 +24,58 @@ class AStarSearch(BaseSearch):
         super().__init__()
     
     def search(self, problem: SearchProblem) -> List[SearchNode]:
+        self.path = []
+        self.frontier = []
+        self.explored = []
+
+        initial_state = problem.get_initial_state()
+
+        # Calc heuristic for init state
+        heuristic = initial_state.distance_manhattan(problem.goal_state)
+
+        initial_node = AStarNode(initial_state, None, 0.0, heuristic)
+        # A* priority que
+        heapq.heappush(self.frontier, initial_node)
+
+        best_cost = {initial_state: 0.0}
+        
+        while self.frontier:
+            # get node with lowest f(n)
+            current_node = heapq.heappop(self.frontier)
+            current_state = current_node.get_state()
+
+            # skip outdated nodes with a worse cost
+            if current_node.get_cost() > best_cost[current_state]:
+                continue
+
+            #Check if we already reached the goal
+            if problem.is_goal_state(current_state):
+                self.path = current_node.get_path_from_root()
+                return self.path
+
+            self.explored.append(current_node)
+
+            # Get all possible successors
+            successors = problem.get_successors(current_state)
+
+            for successor in successors:
+                new_cost = current_node.get_cost() + 1
+                
+                # Add successor if it is new or is reached with a cheaper path
+                if successor not in best_cost or new_cost < best_cost[successor]:
+                    best_cost[successor] = new_cost
+                    
+                    # Manhattan distance is used as heuristic
+                    heuristic = successor.distance_manhattan(problem.goal_state)
+                    node = AStarNode(successor, current_node, new_cost, heuristic)
+
+                    heapq.heappush(self.frontier, node)
+
         return []
-    
     
     def get_frontier_nodes(self) -> List[SearchNode]:
-        return []
-    
+        return list(self.frontier)
+
     def get_explored_nodes(self) -> List[SearchNode]:
-        return []
-    
-    def get_all_expanded_nodes(self) -> List[SearchNode]:
-        return []
+        return list(self.explored)
     
--- lab1-Search/vacuum_world/search/depth_first_search.py
+++ lab1-Search/vacuum_world/search/depth_first_search.py
@@ -3,6 +3,7 @@ from vacuum_world.search.search_node import SearchNode
 from vacuum_world.search.problem import SearchProblem
 from .base_search import BaseSearch
 
+from collections import deque
 
 class DepthFirstSearch(BaseSearch):
 
@@ -10,11 +11,48 @@ class DepthFirstSearch(BaseSearch):
         super().__init__()
     
     def search(self, problem: SearchProblem) -> List[SearchNode]:
+        self.path = []
+            
+        initial_state = problem.get_initial_state()
+        initial_node = SearchNode(initial_state, None, None, 0.0)
+
+        # DFS Stack
+        self.frontier = deque([initial_node])
+
+        # A visited list to keep the algorithm gooing in circles checking the same node over and over agian
+        visited = {initial_state}
+
+        self.explored = []
+
+        steps = 0
+            
+        while self.frontier and steps < self.max_depth:
+            current_node = self.frontier.pop()
+            current_state = current_node.get_state()
+
+            # Check if we've reached the goal
+            if problem.is_goal_state(current_state):
+                self.path = current_node.get_path_from_root()
+                return self.path
+
+            self.explored.append(current_node)
+
+            # Get all possible successors
+            successors = problem.get_successors(current_state)
+
+            for successor in successors:
+                if successor not in visited:
+                    node = SearchNode(successor, current_node, None, current_node.get_cost() + 1)
+                    self.frontier.append(node)
+                    visited.add(successor)
+
+            steps += 1
+
         return []
     
     
     def get_frontier_nodes(self) -> List[SearchNode]:
-        return []
+        return list(self.frontier)
     
     def get_explored_nodes(self) -> List[SearchNode]:
-        return []
+        return list(self.explored)
